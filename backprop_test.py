9a5bc1e939694d696e7ead00d59dbe141fd1121c
Hadleigh-Schwartz <hadleigh.schwartz@gmail.com>
2024-12-17 12:51:21 -0500

    backprop testing

--- backprop_test.py
+++ backprop_test.py
@@ -2,18 +2,22 @@ import torch
 import torch.optim as optim
 from dynamic_features_differentiable import VeriLightDynamicFeatures
 from hadleigh_utils import aggregate_video_frames
-import matplotlib.pyplot as plt
-
 torch.autograd.set_detect_anomaly(True)
-frames = aggregate_video_frames("data/test_video1.mp4", 90)
+
+frames = aggregate_video_frames("data/test_video1.mp4", 90) # read in video as 4D tensor
+
+# initialize opt and my model
 model = VeriLightDynamicFeatures(long_range_face_detect=False, short_range_face_detect=False)
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+# run a forward pass and backward pass
 pred_vec = model(frames)
 loss = torch.nn.MSELoss()(pred_vec, torch.zeros_like(pred_vec))
 optimizer.zero_grad()
 loss.backward()
 optimizer.step()
-# Print gradients for each parameter in your model
+
+# print gradients
 for name, param in model.named_parameters():
     print(name, param.grad)
 
