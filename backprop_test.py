bb645baab318796b81c770f75e11c30deb041c7b
Hadleigh-Schwartz <hadleigh.schwartz@gmail.com>
2024-12-17 12:50:24 -0500

    backprop testing

--- backprop_test.py
+++ backprop_test.py
@@ -1,47 +1,20 @@
-
-
-
-# read in a video
-# create a dummy vector of same dimensionality of our dynamic one
-# pass video htyrough the model, which just returns the frames exactly
-# loss function compute dynamic feature vec 
-# compare with dummy
-
 import torch
-import torch.nn as nn
-import torchvision
-import matplotlib.pyplot as plt
 import torch.optim as optim
 from dynamic_features_differentiable import VeriLightDynamicFeatures
-import torch
-import cv2
-from torchviz import make_dot
 from hadleigh_utils import aggregate_video_frames
 import matplotlib.pyplot as plt
 
-
-class Dummy(nn.Module):
-    def __init__(self):
-        super().__init__()
-        
-    def forward(self, x):
-        return x
- 
-vl = VeriLightDynamicFeatures(long_range_face_detect=False, short_range_face_detect=False)
-def vl_loss(x, vec):
-    dynamic_feature_vec = vl(frames)
-    loss = torch.nn.MSELoss()(dynamic_feature_vec, vec)
-    return loss
-
 torch.autograd.set_detect_anomaly(True)
 frames = aggregate_video_frames("data/test_video1.mp4", 90)
-loss = vl_loss(frames, torch.zeros(1440))
+model = VeriLightDynamicFeatures(long_range_face_detect=False, short_range_face_detect=False)
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+pred_vec = model(frames)
+loss = torch.nn.MSELoss()(pred_vec, torch.zeros_like(pred_vec))
+optimizer.zero_grad()
 loss.backward()
-print(loss)
-
-# optimizer.zero_grad()
-# loss.backward()
-# optimizer.step()
-
+optimizer.step()
+# Print gradients for each parameter in your model
+for name, param in model.named_parameters():
+    print(name, param.grad)
 
 
